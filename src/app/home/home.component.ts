f15698505199790bc720ee1f7fb61dff12e1337c
nikolasokolov <35638588+nikolasokolov@users.noreply.github.com>
2019-11-16 14:13:28 +0100

    Added test api for api gateway

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,8 @@ import {AuthenticationService} from '../authentication/authentication.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  testApi = null;
+  testApiSuccess = true;
 
   constructor(private httpClient: HttpClient, private authenticationService: AuthenticationService) { }
 
@@ -17,8 +19,11 @@ export class HomeComponent implements OnInit {
   test() {
     this.httpClient.get('https://localhost:8080/api/test').subscribe(response => {
       console.log('success');
+      this.testApi = 'Successfully received response from test api';
     }, error => {
       console.log('error');
+      this.testApi = 'Error occurred during execution of test api';
+      this.testApiSuccess = false;
     });
   }
 
