291318e3b84a198a78d26e9032a8d3a7aa9579a6
nikolasokolov <35638588+nikolasokolov@users.noreply.github.com>
2019-11-19 22:08:07 +0100

    Added nested observable to get user details after login

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,10 +1,12 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, pipe} from 'rxjs';
 import {User} from '../model/user.model';
 import {HttpClient} from '@angular/common/http';
-import {tap} from 'rxjs/operators';
+import {switchMap, tap} from 'rxjs/operators';
 import {Router} from '@angular/router';
 import {AuthenticationResponseData} from '../model/authentication-response-data.model';
+import {UserDetails} from '../model/user-details.model';
+import {Company} from '../model/company.model';
 
 @Injectable({providedIn: 'root'})
 export class AuthenticationService {
@@ -23,7 +25,7 @@ export class AuthenticationService {
           +response.expiresIn
         );
       })
-    );
+    ).pipe(switchMap(() => this.getUser()));
   }
 
   isAuthenticated() {
@@ -38,6 +40,19 @@ export class AuthenticationService {
     localStorage.setItem('Authorization', token);
     this.autoLogout(expiresIn * 1000);
     this.router.navigate(['/home']);
+  }
+
+  getUser() {
+    return this.httpClient.get<UserDetails>('https://localhost:8080/api/account')
+      .pipe(tap(response => {
+      this.handleUserDetails(response.id, response.username, response.password, response.email,
+        response.authorities, response.company);
+    }));
+  }
+
+  handleUserDetails(id: number, username: string, password: string, email: string, authorities: any[], company: Company) {
+    const userDetails = new UserDetails(id, username, password, email, authorities, company);
+    localStorage.setItem('userDetails', JSON.stringify(userDetails));
   }
 
   autoLogout(expirationDuration: number) {
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,49 +1,36 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthenticationService} from '../authentication/authentication.service';
-import {BehaviorSubject, Subscription} from 'rxjs';
+import {BehaviorSubject, Observable, Subscription} from 'rxjs';
 import {UserDetails} from '../model/user-details.model';
 import {HttpClient} from '@angular/common/http';
 import {exhaustMap, take} from 'rxjs/operators';
+import {User} from '../model/user.model';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit, OnDestroy {
-  isAuthenticated = false;
-  loggedUserDetails = {};
+export class NavbarComponent implements OnInit {
   private userSub: Subscription;
+  isAuthenticated = false;
+  userDetails = {};
 
-  constructor(private authenticationService: AuthenticationService, private httpClient: HttpClient) { }
+  constructor(private authenticationService: AuthenticationService, private httpClient: HttpClient) {
+  }
 
   ngOnInit() {
     this.userSub = this.authenticationService.user.subscribe(user => {
-      if (user !== null) {
-        this.isAuthenticated = !!user;
-        this.getUser();
-      } else {
-        this.isAuthenticated = false;
-      }
+      this.isAuthenticated = !!user;
     });
+    if (this.isAuthenticated) {
+      this.userDetails = JSON.parse(localStorage.getItem('userDetails'));
+      console.log(this.userDetails);
+    }
   }
 
   logout() {
     this.authenticationService.logout();
   }
 
-  getUser() {
-    return this.httpClient.get<UserDetails>('https://localhost:8080/api/account').subscribe(response => {
-      const userDetails = new UserDetails(response.id, response.username, response.password,
-        response.email, response.authorities, response.company);
-      this.loggedUserDetails = userDetails;
-      if (userDetails !== null) {
-        localStorage.setItem('userDetails', JSON.stringify(userDetails));
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    this.userSub.unsubscribe();
-  }
 }
