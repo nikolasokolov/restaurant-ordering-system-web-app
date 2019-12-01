df0126585d67981d7f04fb968263c847143fbad8
nikolasokolov <35638588+nikolasokolov@users.noreply.github.com>
2019-12-01 16:46:16 +0100

    Removed test api gateway

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,23 +8,11 @@ import {AuthenticationService} from '../authentication/authentication.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  testApi = null;
-  testApiSuccess = true;
 
-  constructor(private httpClient: HttpClient, private authenticationService: AuthenticationService) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  test() {
-    this.httpClient.get('https://localhost:8080/main/test').subscribe(response => {
-      console.log('success');
-      this.testApi = 'Successfully received response from test api';
-    }, error => {
-      console.log('error');
-      this.testApi = 'Error occurred during execution of test api';
-      this.testApiSuccess = false;
-    });
-  }
 
 }
